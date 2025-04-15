8fd25cab3f1f033ffbfa3492b04002eee38b7297
Bruno Cerqueira <bruno.c.cerqueira@gmail.com>
2025-04-15 20:37:30 +0100

    Update epopeia_app.py

--- epopeia_app.py
+++ epopeia_app.py
@@ -19,7 +19,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: url("https://raw.githubusercontent.com/bcc75/epopeIA/main/fundo.jpg");
+        background-image: url("https://raw.githubusercontent.com/bcc75/epopeIA/main/poema.jpg");
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
